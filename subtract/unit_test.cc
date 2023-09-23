3656c71eee2289c07681b77fd9305e043a2e0d82
ka <todo1@rotarydef.net>
2023-09-23 10:46:34 -0700

    look up functions

--- subtract/unit_test.cc
+++ subtract/unit_test.cc
@@ -2,55 +2,87 @@
 #ifdef TMAIN_KAUTIL_FILTER_ALTHMETIC_SUBTRACT_SHARED
 
 #include <stdio.h>
+#include <cstring>
+#include "kautil/algorithm/btree_search/btree_search.h"
+#include <algorithm>
 
-struct filter_handler{
-    
-};
-
+struct filter;
+struct filter_handler{ filter * last=0; };
 struct filter{
-    const char * filer_main_k="filter_main";
-    int (*filer_main_v)(filter_handler * hdl);
+    int (*main)(filter * m,filter_handler * hdl);
 } __attribute__((aligned(8)));
 
-extern "C" int filter_main(filter_handler * hdl) {
+struct filter_lookup_table;
+void * filter_lookup(filter_lookup_table * flookup_table,const char * key){
+    
+    struct Dum{ char* dum[2]; };
+    auto beg = reinterpret_cast<Dum*>(flookup_table);
+    auto buf = reinterpret_cast<Dum*>(flookup_table);
+    while(buf->dum[0]) ++buf;
+    auto len = (buf-beg);
+    auto sortf = [](Dum const& l, Dum const& r){ return !strcmp(l.dum[0],r.dum[0]); };
+    if(!std::is_sorted(beg,beg+len,sortf)) std::sort(beg,beg+len,sortf);
+    
+    auto fp = (char**)0;
+    auto btree = reinterpret_cast<char**>(flookup_table);
+    if(0==kautil_bt_search_v3((void ** )&fp,(const void*)&key,btree,btree+len,sizeof(char*)*2, nullptr,[](auto l,auto r,auto arg){
+        return strcmp(*(const char **)l,*(const char **)r);
+    })){
+        return reinterpret_cast<void*>(fp[1]);
+    }
+    return nullptr;
+}
+
+
+struct subtract_o{};
+struct subtract_i{};
+struct filter_lookup_elem{
+    const char * key = 0; 
+    void * value = nullptr;
+};
+
+
+struct filter_lookup_table{
+    filter_lookup_elem main{.key="fmain"};
+    filter_lookup_elem output{.key="output"};
+    filter_lookup_elem input{.key="input"};
+    filter_lookup_elem sentinel{.key=nullptr,.value=nullptr};
+} __attribute__((aligned(8)));
+
+
+extern "C" int fmain(filter * m,filter_handler * hdl) {
+    hdl->last = m;
     printf("AAA");
     return 0;
 }
 
-filter * filter_initialize(){ 
-    auto res= new filter{}; 
-    res->filer_main_v = filter_main;
+filter_lookup_table * filter_lookup_table_initialize(){ 
+    auto res= new filter_lookup_table{}; 
+    res->main.value = (void*)fmain;
+    res->output.value = new subtract_o;
+    res->input.value = new subtract_i;
     return res;
 }
-void filter_free(filter * f){ delete f; }
+
+void filter_lookup_table_free(filter_lookup_table * f){ 
+    delete reinterpret_cast<subtract_o*>(f->output.value);
+    delete reinterpret_cast<subtract_i*>(f->input.value);
+    delete f; 
+}
 
 
-
-
-#include <cstring>
-#include "kautil/algorithm/btree_search/btree_search.h"
 
 
 int main(){
+    
+    auto f = filter{};
     auto fhdl = filter_handler{};
-    auto f = filter_initialize();
-    auto btree = reinterpret_cast<const char **>(f);
-    printf("%s\n",btree[0]);
-
-    auto key = "filter_main";
-    auto fp = (char**)0;
-
-    auto len = sizeof(*f)/sizeof(uintptr_t);
-    if(0==kautil_bt_search_v3((void ** )&fp,(const void*)&key,btree,btree+sizeof(*f)/sizeof(uintptr_t),sizeof(uintptr_t)*2, nullptr,[](auto l,auto r,auto arg){
-        return strcmp(*(const char **)l,*(const char **)r);
-    })){
-        printf("+++%llx\n",(uintptr_t)*fp);
-        reinterpret_cast<int(*)(filter_handler*)>(fp[1])(&fhdl);
-    }else{
-        printf("fail.");
+    auto flookup = filter_lookup_table_initialize();
+    f.main= (decltype(f.main))filter_lookup(flookup,"fmain");
+    if(f.main){
+        f.main(&f,&fhdl);
     }
-    
-    
+    filter_lookup_table_free(flookup);
     
     
     return 0;

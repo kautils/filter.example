b94c206e31d611af407bc9ab3f4c57476f89d990
ka <todo1@rotarydef.net>
2023-09-24 20:11:05 -0700

    database

--- subtract/subtract.cmake
+++ subtract/subtract.cmake
@@ -11,6 +11,30 @@ git_clone(https://raw.githubusercontent.com/kautils/CMakeLibrarytemplate/v0.0.1/
 git_clone(https://raw.githubusercontent.com/kautils/CMakeGitCurrentCommitHash/v0.0.1/CMakeGitCurrentCommitHash.cmake)
 git_clone(https://raw.githubusercontent.com/kautils/CMakeFetchKautilModule/v0.0.1/CMakeFetchKautilModule.cmake)
 
+
+
+include(ProcessorCount)
+ProcessorCount(${m}_thread_cnt)
+CMakeFetchKautilModule(virtual_file
+        GIT https://github.com/kautils/virtual_file.git
+        REMOTE origin
+        TAG v0.0.1
+        CMAKE_CONFIGURE_MACRO -DCMAKE_CXX_FLAGS="-O2" -DCMAKE_CXX_STANDARD=23
+        CMAKE_BUILD_OPTION -j ${${m}_thread_cnt}
+        )
+find_package(KautilCacheVirtualFile.0.0.1.shared REQUIRED)
+
+CMakeFetchKautilModule(sqlite
+        GIT https://github.com/kautils/sqlite3.git
+        REMOTE origin
+        TAG v2.0.1.0
+        CMAKE_CONFIGURE_MACRO -DCMAKE_CXX_FLAGS="-O2" -DCMAKE_CXX_STANDARD=23
+        CMAKE_BUILD_OPTION -j ${${m}_thread_cnt}
+#        FORCE_UPDATE
+        )
+find_package(KautilSqlite3.2.0.1.0.shared REQUIRED)
+
+
 set(module_name subtract)
 unset(srcs)
 file(GLOB srcs ${CMAKE_CURRENT_LIST_DIR}/*.cc)
@@ -27,32 +51,32 @@ set(${module_name}_common_pref
     DESTINATION_LIB_DIR lib
 )
 
-
 list(APPEND ${m}_unsetter ${m}_thread_cnt)
 
-include(ProcessorCount)
-ProcessorCount(${m}_thread_cnt)
-CMakeFetchKautilModule(c11_string_allocator
-        GIT https://github.com/kautils/btree_search.git
-        REMOTE origin 
-        TAG v0.0.1
-        CMAKE_BUILD_OPTION -j ${${m}_thread_cnt}
-        FORCE_UPDATE
-        )
-
-find_package(KautilAlgorithmBtreeSearch.0.0.1.static REQUIRED)
 
 CMakeGitCurrentCommitHash(${m}_filter_id)
 string(SUBSTRING "${${m}_filter_id}" 0 7 ${m}_filter_short_id)
 set(${m}_filter_hr_id ${PROJECT_NAME}/${${m}_filter_short_id}) # hr_id : human readable id
 CMakeLibraryTemplate(${module_name} EXPORT_LIB_TYPE shared ${${module_name}_common_pref} )
-#CMakeLibraryTemplate(${module_name} EXPORT_LIB_TYPE static ${${module_name}_common_pref} )
+target_link_libraries(${${module_name}_shared} PRIVATE kautil::sqlite3::2.0.1.0::shared)
+
+target_compile_definitions(${${module_name}_shared} PUBLIC
+    -DFILTER_ID_SHORT="${${m}_filter_short_id}"
+    -DFILTER_ID_HR="${${m}_filter_hr_id}"
+    -DFILTER_ID="${${m}_filter_id}"
+)
+
 
 
 set(__t ${${module_name}_shared_tmain})
 add_executable(${__t})
 target_sources(${__t} PRIVATE ${CMAKE_CURRENT_LIST_DIR}/unit_test.cc)
-target_link_libraries(${__t} PRIVATE ${${module_name}_shared} kautil::argorithm::btree_search::0.0.1::static)
+target_link_libraries(${__t} PRIVATE ${${module_name}_shared} 
+        kautil::sqlite3::2.0.1.0::shared 
+#        kautil::cache::virtual_file::0.0.1::shared
+        )
+
+
 target_compile_definitions(${__t} PRIVATE ${${module_name}_shared_tmain_ppcs})
 
 foreach(__v ${${m}_unsetter})
--- subtract/unit_test.cc
+++ subtract/unit_test.cc
@@ -3,31 +3,227 @@
 
 #include <stdio.h>
 #include <cstring>
-#include "kautil/algorithm/btree_search/btree_search.h"
-#include <algorithm>
+#include <vector>
+#include <string>
+#include "kautil/cache/virtual_file/virtual_file.h"
+#include "kautil/sqlite3/sqlite3.h"
+#include "sys/stat.h"
+#include "libgen.h"
 
 struct filter;
 struct filter_handler{ 
-    filter * previous=0; 
-    filter * last=0; 
+    filter * previous=0;
+    filter * current=0;
+    std::vector<filter*> filters;
+    std::string local_uri;
+    uint64_t io_len=0; // input_len not output_len. all should be the same.
 };
 
+
+struct filter_database_handler{
+    void *(*alloc)()=0;
+    void (*free)(void * hdl)=0;
+    int (*set_uri)(void * hdl,const char * prfx,const char * id)=0;
+    int (*set_up_database)(void * hdl)=0;
+    int (*set_output)(void * hdl,void * begin,void * end)=0;
+    int (*set_input)(void * hdl,void * begin,void * end)=0;
+    int (*set_io_length)(void * hdl,uint64_t)=0;
+    int (*save)(void * hdl,uint64_t const& io_len
+            ,void * begin_i,void * end_i
+            ,void * begin_o,void * end_o)=0;
+    
+    void * hdl=0;
+};
+
+void* filter_input(filter * f);
+uint64_t filter_input_bytes(filter * f);
 struct filter{
-    int (*main)(filter * m,filter_handler * hdl);
+    int (*main)(filter * m)=0;
     void * (*output)(filter *)=0;
     uint64_t(*output_bytes)(filter *)=0;
-    void * (*input)(filter *)=0;
-    uint64_t(*input_bytes)(filter *)=0;
+    void * (*input)(filter *)=filter_input;
+    uint64_t(*input_bytes)(filter *)=filter_input_bytes;
+    const char *(*id)(filter *)=0;
+    const char *(*id_hr)(filter *)=0;
     void* m=0;
+    void* database=0;
+    filter_database_handler * db=0;
     filter_handler * hdl=0;
-    
 } __attribute__((aligned(8)));
-
 ///@note input is common
-void* filter_input(filter * f) { return f->hdl->last->output(f->hdl->last); }
+void* filter_input(filter * f) { return f->hdl->previous->output(f->hdl->previous); }
 uint64_t filter_input_bytes(filter * f) { 
-    return f->hdl->previous->output_bytes(f->hdl->previous); 
+    if(f->hdl->previous)return f->hdl->previous->output_bytes(f->hdl->previous); 
+    return 0;
 }
+
+
+
+
+static const char * kCreateSt = "create table if not exists m([rowid] primary key,[k] blob,[v] blob,unique([k])) ";
+static const char * kInsertSt = "insert or ignore into m(k,v) values(?,?)";
+
+
+struct io_data{
+    void * begin;
+    void * end;
+    uint64_t io_length;
+};
+
+struct filter_database_sqlite3_handler{
+    kautil::database::Sqlite3Stmt * create;
+    kautil::database::Sqlite3Stmt * insert;
+    kautil::database::Sqlite3 * sql=0;
+    kautil::database::sqlite3::Options * op=0;
+    std::string uri_prfx;
+    std::string id;
+    
+    io_data i;
+    io_data o;
+    uint64_t io_len=0;
+};
+
+
+void* filter_database_sqlite_alloc(){
+    auto res = new filter_database_sqlite3_handler;
+    res->op = kautil::database::sqlite3::sqlite_options(); 
+    return res;
+}
+
+
+int filter_database_sqlite_set_uri(void * hdl,const char * prfx,const char * id){
+    auto m=reinterpret_cast<filter_database_sqlite3_handler*>(hdl);
+    m->uri_prfx = prfx; 
+    m->id = id; 
+    return 0;
+}
+
+void filter_database_sqlite_free(void* hdl){
+    auto m=reinterpret_cast<filter_database_sqlite3_handler*>(hdl);
+    delete m->op;
+    delete m->sql;
+    delete m;
+}
+
+int filter_database_sqlite_setup(void * hdl){
+    auto db=reinterpret_cast<filter_database_sqlite3_handler*>(hdl);
+    if(!db->sql) {
+        auto path = db->uri_prfx +"/" + db->id + ".sqlite";
+        {
+            struct stat st;
+            auto buf = std::string(path.data());
+            auto dir = dirname(buf.data());
+            if(stat(dir,&st)){
+                if(mkdir(dir)){
+                    printf("fail");
+                    return 1;
+                }
+            } 
+        }
+        db->op = kautil::database::sqlite3::sqlite_options(); 
+        db->sql = new kautil::database::Sqlite3{path.data(),db->op->sqlite_open_create()|db->op->sqlite_open_readwrite()|db->op->sqlite_open_nomutex()};
+        db->create = db->sql->compile(kCreateSt);
+        db->insert = db->sql->compile(kInsertSt);
+    }
+    return 0;
+}
+
+
+int set_output(void * hdl,void * begin,void * end){
+    auto m=reinterpret_cast<filter_database_sqlite3_handler*>(hdl);
+    m->o.begin = begin;
+    m->o.end = end;
+    return m->o.begin < m->o.end; 
+}
+
+int set_input(void * hdl,void * begin,void * end){
+    auto m=reinterpret_cast<filter_database_sqlite3_handler*>(hdl);
+    m->i.begin = begin;
+    m->i.end = end;
+    return m->i.begin < m->i.end; 
+}
+
+int set_io_length(void * hdl,uint64_t len){
+    auto m=reinterpret_cast<filter_database_sqlite3_handler*>(hdl);
+    m->io_len= len;
+    return 0;
+}
+
+int filter_execute(void * hdl){
+    auto m=reinterpret_cast<filter_database_sqlite3_handler*>(hdl);
+    m->create->step(true);
+    if(auto begin_i = reinterpret_cast<char*>(m->i.begin)){
+        auto end_i = reinterpret_cast<char*>(m->i.end);
+        auto block_i = (end_i - begin_i) / m->io_len;
+        if(auto begin_o = reinterpret_cast<const char*>(m->o.begin)){
+            auto end_o = reinterpret_cast<const char*>(m->o.end);
+            auto block_o = (end_o - begin_o) / m->io_len;
+            auto fail = false;
+            m->sql->begin_transaction();
+            for(;begin_i != end_i; begin_i+=block_i,begin_o+=block_o){
+                auto res_stmt = !m->insert->set_blob(1,begin_i,block_i) + !m->insert->set_blob(2,begin_o,block_o);
+                auto res_step = m->insert->step(true);
+                res_step = res_step != m->op->sqlite_ok();
+                if((fail=res_stmt+res_step))break;
+            }
+            if(fail) m->sql->roll_back();
+            m->sql->end_transaction();
+            return 0;
+        }
+        return 1;
+    }else m->sql->error_msg();
+    return 1;
+}
+
+
+int filter_database_sqlite(filter* f){
+    auto db=reinterpret_cast<filter_database_sqlite3_handler*>(f->database);
+    if(!db->sql) {
+        auto path = f->hdl->local_uri +"/" + f->id_hr(f) + ".sqlite";
+        {
+            struct stat st;
+            auto buf = std::string(path.data());
+            auto dir = dirname(buf.data());
+            if(stat(dir,&st)){
+                if(mkdir(dir)){
+                    printf("fail");
+                    return 1;
+                }
+            } 
+        }
+        db->op = kautil::database::sqlite3::sqlite_options(); 
+        db->sql = new kautil::database::Sqlite3{path.data(),db->op->sqlite_open_create()|db->op->sqlite_open_readwrite()|db->op->sqlite_open_nomutex()};
+        db->create = db->sql->compile(kCreateSt);
+        db->insert = db->sql->compile(kInsertSt);
+    }
+
+
+    db->create->step(true);
+    if(auto begin_i = reinterpret_cast<char*>(f->input(f))){
+        auto end_i = reinterpret_cast<char*>(f->input(f)) + f->input_bytes(f);
+        auto block_i = (end_i - begin_i) / f->hdl->io_len;
+
+        if(auto begin_o = reinterpret_cast<const char*>(f->output(f))){
+            auto end_o = reinterpret_cast<const char*>(f->output(f)) + f->output_bytes(f);
+            auto block_o = (end_o - begin_o) / f->hdl->io_len;
+            auto fail = false;
+            db->sql->begin_transaction();
+            for(;begin_i != end_i; begin_i+=block_i,begin_o+=block_o){
+                auto res_stmt = !db->insert->set_blob(1,begin_i,block_i) + !db->insert->set_blob(2,begin_o,block_o);
+                auto res_step = db->insert->step(true);
+                res_step = res_step != db->op->sqlite_ok();
+                if((fail=res_stmt+res_step))break;
+            }
+            if(fail) db->sql->roll_back();
+            db->sql->end_transaction();
+            return 0;
+        }
+        return 1;
+    }else db->sql->error_msg();
+    return 1;
+}
+
+
 
 
 struct filter_lookup_table;
@@ -40,20 +236,21 @@ void * filter_lookup(filter_lookup_table * flookup_table,const char * key){
     // sorting is bad for this process. i neither want  not to change the order of the flookup_table nor to copy it 
     auto arr = reinterpret_cast<char **>(flookup_table);
     for(;;arr+=sizeof(filter_lookup_elem)/sizeof(uintptr_t)){
-        if(!strcmp(key,arr[0]))return reinterpret_cast<void*>(arr[1]);
         if(nullptr==arr[0]) break;
+        if(!strcmp(key,arr[0]))return reinterpret_cast<void*>(arr[1]);
     }
     return nullptr;
 }
 
 
 
-struct subtract_o{}__attribute__((aligned(8)));
-struct subtract_i{}__attribute__((aligned(8)));
+
+
+
 
 struct subtract{
-    subtract_o o;
-    subtract_o i;
+    std::vector<double> res;
+    uint64_t len=0;
 };
 
 #define m(object) reinterpret_cast<subtract*>(object->m)
@@ -64,19 +261,36 @@ struct filter_lookup_table{
     filter_lookup_elem output_bytes{.key="output_bytes"};
     filter_lookup_elem input{.key="input"};
     filter_lookup_elem input_bytes{.key="input_bytes"};
+    filter_lookup_elem id{.key="id"};
+    filter_lookup_elem id_hr{.key="id_hr"};
     filter_lookup_elem member{.key="member"};
     filter_lookup_elem sentinel{.key=nullptr,.value=nullptr};
 } __attribute__((aligned(8)));
 
-extern "C" int fmain(filter * m,filter_handler * hdl) {
-    hdl->previous = hdl->last; 
-    hdl->last = m;
-    printf("AAA\n");
+extern "C" int fmain(filter * f) {
+    auto arr = reinterpret_cast<double*>(f->input(f));
+    auto len = f->input_bytes(f)/sizeof(double);
+    
+    auto a = m(f)->len;
+    m(f)->res.push_back(100);
+    
+    if(m(f)->res.size() < len)m(f)->res.resize(len);
+    m(f)->len=len;
+    for(auto i = 0; i < len ; i++){
+        m(f)->res[i] = arr[i];
+    }
     return 0;
 }
 
-extern "C" uint64_t output_bytes(filter * f) { return sizeof(subtract_o); }
-extern "C" void* output(filter * f) { return &m(f)->o; }
+
+#define FILTER_DATABASE_TYPE_SQLITE3 0
+
+
+extern "C" uint64_t output_bytes(filter * f) { return m(f)->res.size()*sizeof(double); }
+extern "C" void* output(filter * f) { return m(f)->res.data(); }
+extern "C" const char* id(filter * f) { return FILTER_ID; }
+extern "C" const char* id_hr(filter * f) { return FILTER_ID_HR; }
+extern "C" int database_type(filter * f){ return FILTER_DATABASE_TYPE_SQLITE3; }
 
 
 filter_lookup_table * filter_lookup_table_initialize(){ 
@@ -84,6 +298,8 @@ filter_lookup_table * filter_lookup_table_initialize(){
     res->main.value = (void*)fmain;
     res->output.value = (void*)output;
     res->output_bytes.value=(void*)output_bytes;
+    res->id.value=(void*)id;
+    res->id_hr.value=(void*)id_hr;
     res->member.value = new subtract{};
     return res;
 }
@@ -93,88 +309,100 @@ void filter_lookup_table_free(filter_lookup_table * f){
     delete f; 
 }
 
-int cnt = 0;
-bool __mock_if(bool cond) {
-    // assume this return 0/1. possible to express with small code 
-    return cnt++;
-}
-
 
 #define __begin_cycle
 #define __end_cycle
 
 
+
+struct filter_first{
+    void * o=0;
+    uint64_t o_bytes=0;
+    uint64_t o_length=0;
+}__attribute__((aligned(8)));
+
+void* filter_first_output(filter * f){
+    return reinterpret_cast<filter_first*>(f->m)->o;
+}
+uint64_t filter_first_output_bytes(filter * f){
+    return reinterpret_cast<filter_first*>(f->m)->o_bytes;
+}
+
+uint64_t filter_first_output_length(filter * f){
+    return reinterpret_cast<filter_first*>(f->m)->o_length;
+}
+
+
+
 int main(){
     
-// this is generated by external tool such as cmake, bazel or other. it should be simple text copy/insert process.
-#define if(abc) if(__mock_if(abc))
-    // cycle to allocate memocy. i doubt this is not optimized completely by gcc/clang.
-    // below may make compiler confuse because of mock if. but mock if is important for this program. 
-    {
-        for(auto i = 0; i < 2; ++i){
-            int abc = 0;
-            if(!abc){ // mock if
-                printf("AAAAAAA\n");
-            }else{
-                printf("BBBBBBB\n");
-            }
+    auto input_len = 100; // all the input/output inside a chain is the same,if the result structure are counted as one data 
+    auto i = new filter_first{};
+    filter input;{
+        {
+            auto arr = new double[input_len];
+            for(auto i = 0; i < input_len; ++i)arr[i] = i;
+            i->o = arr;
+            i->o_bytes = sizeof(double)*input_len; 
+            i->o_length = input_len; 
         }
+        input.m = i;
+        input.output=filter_first_output;
+        input.output_bytes=filter_first_output_bytes;
     }
-#undef if
-
-
-__begin_cycle // detected by generators(e.g cmake)
-    {
-        // cycle to run. i think this can be purely optimized by gcc/clang.
-        int abc = 0;
-        if(!abc){ // pure(ordinal) if
-            printf("AAAAAAA\n");
-        }else{
-            printf("BBBBBBB\n");
-        }
-    }
-__end_cycle
     
     
-//    struct{
-//        double a = 123.4;
-//        double b = 567.8;
-//    }input __attribute__((aligned(8)));
-//    
-//    filter inputer;
-//
-//    
-//    using output_t = void*(*)(filter *);
-//    using input_t = void*(*)(filter *);
-//    using output_bytes_t = uint64_t(*)(filter *);
-//    using input_bytes_t = uint64_t(*)(filter *);
-//    
-//    auto f = filter{};
-//    auto fhdl = filter_handler{};
-//    auto flookup = filter_lookup_table_initialize();
-//    f.main= (decltype(f.main))filter_lookup(flookup,"fmain");
-//    f.output= (output_t)filter_lookup(flookup,"output");
-//    f.output_bytes= (output_bytes_t)filter_lookup(flookup,"output_bytes");
-//    f.input= (input_t)filter_lookup(flookup,"input");
-//    f.input= (input_t) filter_input;
-//    f.input_bytes= (input_bytes_t) filter_input_bytes;
-//    f.hdl=&fhdl;
-//    
-//    
-//    if(f.main){
-//        f.main(&f,&fhdl);
-//    }
-//    if(f.output_bytes){
-//        printf("%lld\n",f.output_bytes(&f));
-//    }
-//    if(f.input_bytes){
-//        
-//        f.input_bytes(&f);
-//        printf("%lld\n",f.input_bytes(&f));
-//    }
-//    
-//    // 
-//    filter_lookup_table_free(flookup);
+    using output_t = void*(*)(filter *);
+    using output_bytes_t = uint64_t(*)(filter *);
+    using filter_id_t = const char* (*)(filter *);
+
+    auto f = filter{};
+    auto fhdl = filter_handler{};
+    fhdl.io_len = input_len;
+    fhdl.local_uri = "./";
+    
+    auto flookup = filter_lookup_table_initialize();
+    f.main= (decltype(f.main))filter_lookup(flookup,"fmain");
+    f.output= (output_t)filter_lookup(flookup,"output");
+    f.output_bytes= (output_bytes_t)filter_lookup(flookup,"output_bytes");
+    f.id= (filter_id_t)filter_lookup(flookup,"id");
+    f.id_hr= (filter_id_t)filter_lookup(flookup,"id_hr");
+    f.m= filter_lookup(flookup,"member");
+    f.database = new filter_database_sqlite3_handler{};
+    f.hdl=&fhdl;
+    
+    fhdl.filters.push_back(&input);
+    fhdl.filters.push_back(&f);
+    {
+        fhdl.previous= nullptr;
+        fhdl.current= &input;
+    }
+    
+    
+    for(auto i = 1; i < fhdl.filters.size(); ++i){
+        auto f = fhdl.filters[i];
+        f->hdl->previous = f->hdl->current;
+        f->hdl->current = f;
+        
+        f->main(f);
+        if(FILTER_DATABASE_TYPE_SQLITE3==database_type(f)){
+            filter_database_sqlite(f);
+        }
+    }
+    
+    
+    
+    if(f.output_bytes){
+        printf("%lld\n",f.output_bytes(&f));
+    }
+    if(f.input_bytes){
+        f.input_bytes(&f);
+        printf("%lld\n",f.input_bytes(&f));
+    }
+
+    filter_lookup_table_free(flookup);
+    delete [] (double*)i->o;
+    delete i;
     
     
     return 0;

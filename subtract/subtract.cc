af58fd12592289185cfca3457126773e9291abd1
ka <todo1@rotarydef.net>
2023-09-29 23:14:22 -0700

    state

--- subtract/subtract.cc
+++ subtract/subtract.cc
@@ -1,11 +1,23 @@
 #include <vector>
-#include <string>
 #include "flow.h"
+#include <string>
+#include <array>
+#include <numeric>
+
+constexpr int kLengthAdj = 100;
 
 struct subtract{
     std::vector<double> res;
     uint64_t len=0;
+    inline static std::array<uint64_t,100> kV;
+    std::array<uint64_t,100>::iterator v_cur;
+    std::string state_id_buffer;
 };
+
+bool kInit_once = [](){
+    std::iota(subtract::kV.begin(),subtract::kV.end(),0);
+    return true;
+}.operator()();
 
 struct filter_lookup_elem{
     const char * key = 0; 
@@ -14,17 +26,32 @@ struct filter_lookup_elem{
 
 #define m(object) reinterpret_cast<subtract*>(object->m)
 
+
+
+int fmain(filter * f);
+void state_reset(filter * f);
+bool state_next(filter * f);
+const char * state_id(filter * f);
+uint64_t output_bytes(filter * f);
+void* output(filter * f);
+const char* id(filter * f);
+const char* id_hr(filter * f);
+
+
 struct filter_lookup_table_subtract{
-    filter_lookup_elem main{.key="fmain"};
-    filter_lookup_elem output{.key="output"};
-    filter_lookup_elem output_bytes{.key="output_bytes"};
-    filter_lookup_elem database_type{.key="database_type"};
-    filter_lookup_elem id{.key="id"};
-    filter_lookup_elem id_hr{.key="id_hr"};
-    filter_lookup_elem member{.key="member"};
+    filter_lookup_elem main{.key="fmain",.value=(void*)fmain};
+    filter_lookup_elem output{.key="output",.value=(void*)::output};
+    filter_lookup_elem output_bytes{.key="output_bytes",.value=(void*)::output_bytes};
+    filter_lookup_elem id{.key="id",.value=(void*)::id};
+    filter_lookup_elem id_hr{.key="id_hr",.value=(void*)::id_hr};
+    filter_lookup_elem member{.key="member",.value=(void*)new subtract};
+    filter_lookup_elem state_reset{.key="state_reset",.value=(void*)::state_reset};
+    filter_lookup_elem state_next{.key="state_next",.value=(void*)::state_next};
+    filter_lookup_elem state_id{.key="state_id",.value=(void*)::state_id};
     filter_lookup_elem sentinel{.key=nullptr,.value=nullptr};
 } __attribute__((aligned(sizeof(uintptr_t))));
     
+
 int fmain(filter * f) {
     auto arr = reinterpret_cast<double*>(f->input(f));
     auto len = f->input_bytes(f)/sizeof(double);
@@ -41,25 +68,22 @@ int fmain(filter * f) {
 }
 
 
+void state_reset(filter * f){ m(f)->v_cur=m(f)->kV.begin(); }
+const char * state_id(filter * f){ return (m(f)->state_id_buffer=std::to_string(*m(f)->v_cur)).data(); }
+bool state_next(filter * f){ 
+    return m(f)->kV.end() != ++m(f)->v_cur; 
+};
+
+
 uint64_t output_bytes(filter * f) { return m(f)->res.size()*sizeof(double); }
 void* output(filter * f) { return m(f)->res.data(); }
 const char* id(filter * f) { return FILTER_ID; }
 const char* id_hr(filter * f) { return FILTER_ID_HR; }
-int database_type(filter * f){ return FILTER_DATABASE_TYPE_SQLITE3; }
-
 
 
 extern "C" uint64_t size_of_pointer(){ return sizeof(uint64_t); }
 extern "C" filter_lookup_table * lookup_table_initialize(){ 
-    auto res= new filter_lookup_table_subtract{}; 
-    res->main.value = (void*)fmain;
-    res->output.value = (void*)output;
-    res->output_bytes.value=(void*)output_bytes;
-    res->id.value=(void*)id;
-    res->id_hr.value=(void*)id_hr;
-    res->member.value = new subtract{};
-    res->database_type.value = (void *) database_type;
-    return reinterpret_cast<filter_lookup_table*>(res);
+    return reinterpret_cast<filter_lookup_table*>(new filter_lookup_table_subtract);
 }
 
 extern "C" void lookup_table_free(filter_lookup_table * f){
